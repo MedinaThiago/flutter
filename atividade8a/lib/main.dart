bfa220bbcb4b705d35ca8f37410aea22e75c673c
Thiago Medina <thiago1272000@gmail.com>
2020-09-24 21:07:45 -0300

    Atividade 8-1

--- atividade8a/lib/main.dart
+++ atividade8a/lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(home: Home()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        title: Text('Página Inicial'),
       ),
-      home: MyHomePage(),
+      body: Center(
+        child: Text(
+          'Olá, Mundo!!!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blue[900],
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Exemplo de botão',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
